3c6df4866ab724a86d9cf2e7fd7321685d0ec5bd
PauAbril <pau.abril@iesjoandaustria.org>
2020-09-28 12:28:26 +0000

    vuite

--- sete.py
+++ sete.py
@@ -1,2 +1,9 @@
-# formula d'equacions de tercer grau
+# a*x**3+b*x**2+c*x+d
+# hay que calcular polimonio de tercer grado
+a = int(input("Primer numero:	"))
+b = int(input("Segon numero:	"))
+c = int(input("Tercer numero:	"))
+d = int(input("Cuart numero:	"))
+x = int(input("Incognita:		"))
 
+print(a * x**3 + b * x**2 + c * x + d)
---
+++ vuite.py
@@ -0,0 +1,31 @@
+#vendes
+cv=input("")
+tv=input("")
+lv=input("")
+
+#preu
+cp=input("")
+tp=input("")
+lp=input("")
+
+#total
+ct=input("")
+tt=input("")
+lt=input("")
+
+# informe de ventes
+print("---------------------------------------------------------------------")
+print()
+print("Producte		Vendes		Preu		Total")
+print()
+print("---------------------------------------------------------------------")
+print()
+print("Cola			"+cv+"		0,17		170000,00")
+print()
+print("Taronja			"+tv+"		0,20		70000,00")
+print()
+print("Llimona			"+lv+"		0,19		100700,00")
+print()
+print("---------------------------------------------------------------------")
+print()
+print("TOTAL 340700,00")
